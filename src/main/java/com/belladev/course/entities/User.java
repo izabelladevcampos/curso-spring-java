653d922a85686088ee72312d7bd60482f5350547
Izabella Campos <izabellacampos2020@outlook.com>
2021-03-16 10:40:46 -0300

    Classe Order, Instant ISO 8601 - parte 1

--- src/main/java/com/belladev/course/entities/User.java
+++ src/main/java/com/belladev/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.belladev.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User implements Serializable {
@@ -20,6 +23,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 		super();
@@ -73,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override

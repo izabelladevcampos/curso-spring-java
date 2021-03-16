fa4ed402478f643da246e0c5a2b6273a9ebfc3e8
Izabella Campos <izabellacampos2020@outlook.com>
2021-03-16 18:26:34 -0300

    OrderItem, Associação ManyToMany, e atributos extras

--- src/main/java/com/belladev/course/entities/Product.java
+++ src/main/java/com/belladev/course/entities/Product.java
@@ -21,15 +21,14 @@ public class Product implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
 
 	@ManyToMany
-	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {

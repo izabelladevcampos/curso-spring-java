206a08ca0b1b70ba7f49a9817f449fdd993b681a
Izabella Campos <izabellacampos2020@outlook.com>
2021-03-17 23:55:58 -0300

    Métodos total e sub-total

--- src/main/java/com/belladev/course/entities/OrderItem.java
+++ src/main/java/com/belladev/course/entities/OrderItem.java
@@ -10,13 +10,13 @@ import com.belladev.course.entities.pk.OrderItemPK;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name= "tb_order_item")
+@Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
 
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
